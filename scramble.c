fa5377d995d09a41f35ea38973d0b9feb89c6e2e
samitha278 <122595628+samitha278@users.noreply.github.com>
2023-01-18 22:23:20 +0530

    Update scramble.c

--- scramble.c
+++ scramble.c
@@ -19,36 +19,40 @@ int main(){
     char alphabet2[] = "abcdefghijklmnopqrstuvwxyz";
     int i;
     for(i=0;i<26;i++){
-        alphabet1[i] = POINT[i];
-        alphabet2[i] = POINT[i];
+        alphabet1[i] == POINT[i];
+        alphabet2[i] == POINT[i];
+        printf("%c %c %d\n ", alphabet1[i],alphabet2[i],POINT[i] );
     }
-
     
-    int value1;
-    int value2;
+    
+    int value1=0;
+    int value2=0;
     int l;
+    int o;
     int m = strlen(player1);
     int n = strlen(player2);
     for(l=0;l<m;l++){
-        if('a'<player1[l]<'z' || 'A'<player1<'Z'){
+        if('a'<player1[l] || player1[l]<'z' || 'A'<player1[l] || player1[l]<'Z'){
             value1+=player1[l];
+            printf("%d\n",value1);
         }else{
             continue;
         }
         
     }
-    for(l=0;l<m;l++){
-        if('a'<player1[l]<'z' || 'A'<player1<'Z'){
-            value2+=player2[l];
+    for(o=0;o<n;o++){
+        if('a'<player2[o]||player2[o]<'z' || 'A'<player2[o]||player2[o]<'Z'){
+            value2+=player2[o];
+            printf("%d\n",value2);
         }else{
             continue;
         }
     }
     
     if(value1 > value2){
-        printf("player1 win\n");
+        printf("player1 win!\n");
     }else if(value1 < value2){
-        printf("player2 win\n");
+        printf("player2 win!\n");
     }else{
         printf("tie!\n");
     }
