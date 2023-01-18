e6f901c62193cef0ee81ae32c72e4b229d7a4555
samitha278 <122595628+samitha278@users.noreply.github.com>
2023-01-18 21:31:46 +0530

    Create scramble.c

---
+++ scramble.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX_SIZE 100
+
+
+void joinValue();
+int predict();
+int splitInput();
+
+int main(){
+    char player1[MAX_SIZE];
+    char player2[MAX_SIZE];
+    
+    printf("player 1: ");
+    scanf("%s", player1);
+    printf("player 2: ");
+    scanf("%s", player2);
+    
+    joinValue();
+    splitInput();
+    predict();
+    
+    return 0;
+    
+}
+
+int splitInput(){
+    char player1[MAX_SIZE];
+    char player2[MAX_SIZE];
+    int value1;
+    int value2;
+    int l;
+    int m = strlen(player1);
+    int n = strlen(player2);
+    for(l=0;l<m;l++){
+        value1+=player1[l];
+    }
+    for(l=0;l<m;l++){
+        value2+=player2[l];
+    }
+    return value1,value2;
+}
+
+
+void joinValue(){
+    int POINT[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+
+    char alphabet1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char alphabet2[] = "abcdefghijklmnopqrstuvwxyz";
+    int i;
+    for(i=0;i<26;i++){
+        alphabet1[i] = POINT[i];
+        alphabet2[i] = POINT[i];
+    } 
+    
+}
+
+int predict(){
+    if(value1 > value2){
+        printf("player1 win\n");
+    }else if(value1 < value2){
+        printf("player2 win\n");
+    }else{
+        printf("tie!\n");
+    }
+}
+
