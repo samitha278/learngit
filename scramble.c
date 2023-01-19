13eaff4441764cfee0b5532a95dcfc79069daedf
samitha278 <122595628+samitha278@users.noreply.github.com>
2023-01-19 13:22:09 +0530

    Update scramble.c

--- scramble.c
+++ scramble.c
@@ -3,12 +3,29 @@
 #include <ctype.h>
 #define MAX_LIMIT 100
 
+int getValue(char name[]);
+void compare();
+int value1;
+int value2;
+
 
 int main(){
-    char name[MAX_LIMIT];
-    printf("your name: ");
-    scanf("%s", name);
+    char player1[MAX_LIMIT];
+    char player2[MAX_LIMIT];
+    printf("player1: ");
+    scanf("%s", player1);
+    printf("player2: ");
+    scanf("%s", player2);
     
+    value1 = getValue(player1);
+    value2 = getValue(player2);
+    
+    compare();
+    return 0;
+}
+
+int getValue(char name[]){
+
     int POINT[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
     char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
@@ -24,13 +41,26 @@ int main(){
             word[j] = ch;
             
             for(k=0;k<26;k++){
-                if(word[j]==alphabet[k]){value+=POINT[k];}
+                if(word[j]==alphabet[k]){
+                    value+=POINT[k];
+                    
+                }
             }
             j++;
         }
         i++;
     }
-    printf("%s %d",word, value );
+    return value;
     
 }
 
+void compare(){
+
+    if(value1>value2){
+        printf("player1 win!");
+    }else if(value1<value2){
+        printf("player2 win!");
+    }else{
+        printf("tie!");
+    }
+}
