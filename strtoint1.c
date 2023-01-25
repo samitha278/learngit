afde1b8100d5970e626c6fad53c6de3a6e1b10c4
samitha <122595628+samitha278@users.noreply.github.com>
2023-01-25 21:15:37 +0530

    Create strtoint1.c
    still have bugs

---
+++ strtoint1.c
@@ -0,0 +1,40 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int giveInt(char a[]);
+int powerV(int a,int b);
+int num;
+    
+int main(){
+    printf("How many characters in your string: ");
+    scanf("%d", &num);
+    char str[num+1];
+    printf("Type here your string: ");
+    scanf("%s", str);
+    
+    int d = giveInt(str);
+    printf("%d",d);
+    return 0;
+}
+
+int giveInt(char a[]){
+    printf("%d\n", sizeof(a));
+    int i=0;
+    int number=0;
+    while(a[i] != '\0'){
+        if(48<=(int)a[i] && (int)a[i]<=57){
+            int y=;
+            number+=((int)a[i]-48) * powerV(10,y);
+            y++
+        }
+        i++;
+    }
+    return number;    
+}
+int powerV(int a,int b){
+    int x=1;
+    for(int i=0;i<b;i++){
+        x*=a;
+    }
+    return x;
+}
