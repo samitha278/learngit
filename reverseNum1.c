7808eeb4c6f5e9649f719c47ff0a70c11c0fb657
samitha278 <122595628+samitha278@users.noreply.github.com>
2023-01-25 13:44:11 +0530

    Create reverseNum1.c

---
+++ reverseNum1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap(char* a,char* b);
+
+int main(){
+    int u;
+    printf("How many numbers in your number: ");
+    scanf("%i" ,&u);
+    
+    char* x;
+    x = malloc(u);
+    printf("Enter number: ");
+    scanf("%s", x);
+    
+    int n = u/2;
+    for(int i=0;i<n;i++){
+        swap((x+i),(x+u-1-i));
+    }
+    printf("Reverse number: %s",x);
+    free(x);
+    return 0;
+}
+
+int swap(char* a,char* b){
+    char tmp;
+    tmp=*a;
+    *a=*b;
+    *b=tmp;
+}
