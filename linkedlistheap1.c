012181ee4722910dea1f0461acee4d2d9d8853c6
samitha <122595628+samitha278@users.noreply.github.com>
2023-01-26 14:30:05 +0530

    Update linkedlistheap1.c
    no errors but have to optimize code

--- linkedlistheap1.c
+++ linkedlistheap1.c
@@ -12,26 +12,25 @@ int main(){
     node* n=malloc(sizeof(node));
     if(n==NULL)
         return 1;
-    (*n).number = 1;
+    n->number = 1;
     list=n; //link list
      
     n= malloc(sizeof(node));
     if(n==NULL)
         return 1;
-    (*n).number=2;
-    (*list).next=n;  //lint 1
+    n->number=2;
+    list->next=n;  //lint 1
     
     n= malloc(sizeof(node));
     if(n==NULL)
         return 1;
-    (*n).number=3;
-    (*((*list).next)).next=n; //link 2
+    n->number=3;
+    list->next->next=n; //link 2
     
     node* counter=list;
     while(counter!=NULL){
-        node x = *counter;
-        printf("%d", x.number);
-        counter = x.next;
+        printf("%d", counter->number);
+        counter = counter->next;
     }
     return 0;
 }
