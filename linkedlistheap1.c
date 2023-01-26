7edfc57f7d7690dbc7608624784bb904fdfdeee9
samitha <122595628+samitha278@users.noreply.github.com>
2023-01-26 15:21:35 +0530

    Update linkedlistheap1.c
    add new item to linked list this code ugly and have to improve and optimize but no errors. do my task right.

--- linkedlistheap1.c
+++ linkedlistheap1.c
@@ -27,12 +27,27 @@ int main()
         return 1;
     n->number=3;
     list->next->next=n; //link 2
-    
+    list->next->next->next=NULL;
     node* counter=list;
     while(counter!=NULL){
         printf("%d", counter->number);
         counter = counter->next;
     }
-    free(n);
+    n=malloc(sizeof(node));
+    if(n==NULL)
+      return 1;
+    n->number=4;
+    
+    counter = list;
+    for(int i=0;i<3;i++)
+    {
+      counter=counter->next;
+      if(i==1)
+        counter->next=n;
+      if(i==2)
+        counter->next=NULL; 
+    }
+    counter=NULL;
+    n=NULL;
     return 0;
 }
