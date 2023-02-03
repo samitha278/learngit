eb8299e2ca38f0f3b900580de4a6e248ab494eed
samitha <122595628+samitha278@users.noreply.github.com>
2023-02-03 22:19:40 +0530

    Update mergesort1.c
    still not working. not compile errors.

--- mergesort1.c
+++ mergesort1.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 
+int merge(int left[],int right[],int size,int a[]){
+    int n=size/2;
+    int x=size-n;
+    int l=0; int r=0; int i=0;
+    while(l<n && r<x && i<size){
+        if(left[l]<right[r]){
+            a[i]=left[l];
+            l++;
+        }else if(left[l]>right[r]){
+            a[i]=right[r];
+            r++;
+        }
+        i++;
+    }
+    return 0;
+}
+
 int mergesort(int size,int a[]){
     
     if(size==1)
         return 0;
     
     int n=size/2;    
-    int left[n];
-    int right[size-n];
+    int left[n]; int right[size-n];
     
     for(int i =0;i<size;i++){
         if(i<n)
             left[i]=a[i];
         else
-            right[i]=a[i];
+            right[i-n]=a[i];
     }
-    int sizeL=sizeof(left)/sizeof(int);
-    int sizeR=sizeof(right)/sizeof(int);
     
-    mergesort(sizeL,left);
-    mergesort(sizeR,right);
+    mergesort(n,left);
+    mergesort(size-n,right);
     
-    int merge[size*2];
-    for(int i=0;i<size;i++){
-        if(left[i]<right[i]){
-            merge[i]=left[i];
-            merge[i+1]=right[i];
-        }else{
-            merge[i]=right[i];
-            merge[i+1]=left[i];
-        }
-    }
+    merge(left,right,size,a);
+    
     return 0;
 }
-
 
 int main(){
     int a[]={3,2,5,7,0,6,1,4,};
